fb8f6549227941be6533fdfd67b1d58010464b16
Sooooyeon <tn7278@naver.com>
2024-03-20 17:48:31 +0900

    create weather script

--- script/weather.js
+++ script/weather.js
@@ -1,4 +1,25 @@
+import WEATHER_API_KEY from './api.js';
 
-const APT_KEY = env.WEATHER_API_KEY;
 
-console.log(navigator.geolocation.getCurrentPosition());
+console.log(WEATHER_API_KEY);
+
+
+
+// 위치 정보를 가져오는 함수를 정의합니다.
+export const getWeatherInfo = () => {
+  // 위치 정보를 성공적으로 가져왔을 때 실행될 콜백 함수입니다.
+  const onSuccess = (position) => {
+    console.log(position);
+  };
+
+  // 위치 정보를 가져오는 데 실패했을 때 실행될 콜백 함수입니다.
+  const onError = (error) => {
+    console.error(error);
+  };
+
+  // navigator.geolocation.getCurrentPosition() 메서드를 사용하여 위치 정보를 가져옵니다.
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+};
+
+// 위치 정보를 가져오기 위해 함수를 호출합니다.
+getWeatherInfo();
